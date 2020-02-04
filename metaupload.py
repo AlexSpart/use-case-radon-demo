f1f0d2933020862c3101a23df22628147aca490e
naesheim <hg.nesheim@gmail.com>
2020-02-04 16:13:38 +0100

    comment out subpart

--- metaupload.py
+++ metaupload.py
@@ -5,7 +5,7 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-ARTIFACT_TABLE = os.environ['ARTIFACT_TABLE']
+# ARTIFACT_TABLE = os.environ['ARTIFACT_TABLE']
 ARTIFACT_TABLE = 'artifacts-test'
 
 def create_meta(event, context):
