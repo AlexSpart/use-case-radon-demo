c56e51288dfe8bbaf711c2096fc7572e6eb8cc74
naesheim <hg.nesheim@gmail.com>
2020-03-02 10:43:46 +0100

    update curl function

--- ctt/customscript.sh
+++ ctt/customscript.sh
@@ -1,1 +1,1 @@
-curl -X PUT -H "Content-Type: application/zip" -I https://repo.cloudstash.io/radon/functions-public/aws/python3/Test-App/1.0.1/test-app.zip --upload-file "$TEST_FILE"
+curl -H "Content-Type: application/zip" -T "$TEST_FILE" https://repo.cloudstash.io/radon/functions-public/aws/python3/Test-App/1.0.2/test-app.zip
