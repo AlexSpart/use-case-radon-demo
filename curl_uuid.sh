e6a641c2b95fa3fe12c79d4f95ed721a0bd771b0
Alexandros Spartalis <alexandros.spartalis@eficode.com>
2020-06-24 18:13:20 +0200

    Update curl_uuid.sh

--- curl_uuid.sh
+++ curl_uuid.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+echo $PATH
+sh 'export PATH="/home/jenkins/.local/lib/python3.6/site-packages:$PATH"'
+echo $PATH
 
 curl -X POST "$1" -H "accept: */*" -H "Content-Type: application/json" -d "$2" | jq .uuid | tr -d \" 
