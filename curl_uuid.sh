9a2f98e727864431fb0e5930a3dd0f89b8b22e79
Alexandros Spartalis <alexandros.spartalis@eficode.com>
2020-06-25 09:48:54 +0200

    Update curl_uuid.sh

--- curl_uuid.sh
+++ curl_uuid.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
+curl -X POST "$1" -H "accept: */*" -H "Content-Type: application/json" -d "$2" | jq .uuid | tr -d \" 
 
-exmp = (curl -X POST "$1" -H "accept: */*" -H "Content-Type: application/json" -d "$2" | tr -d \")
-def extract_uuid = new JsonSlurper().parseText(exmp).uuid
-echo extract_uuid
 
 
