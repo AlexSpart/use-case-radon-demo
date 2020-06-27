4a3bb1f51d30100b21cb730243dbf8a8b28fb8e5
Alexandros Spartalis <alexandros.spartalis@eficode.com>
2020-06-27 13:10:55 +0200

    Update curl_uuid.sh

--- curl_uuid.sh
+++ curl_uuid.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -X POST "$1" -H "accept: */*" -H "Content-Type: application/json" -d "$2" | /home/jenkins/.local/bin/yq .uuid | tr -d \" 
+curl -X POST "$1" -H "accept: */*" -H "Content-Type: application/json" -d "$2" | /home/jenkins/.local/bin/jq .uuid | tr -d \" 
 
 
 
